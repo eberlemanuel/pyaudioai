2d6397cb0c65911da00b1ec9594c464b50c98f4c
Manuel Eberle <>
2023-06-04 15:53:04 +0200

    release 0.0.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=['openai', 'gTTS', 'playsound', 'pyaudio', 'wave'],
     extras_require={
         'dev': ['check-manifest'],
